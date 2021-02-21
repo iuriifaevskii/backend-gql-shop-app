eb1694c95f6dc77bc6ce29f5693014d76793e117
iuriifaevskii <urfaevskii@gmail.com>
2021-02-21 14:01:43 +0200

    add remote postgres from heroku connection

---
+++ backend/ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  entities: ["src/**/**.entity{.ts,.js}"],
+  synchronize: true,
+}
